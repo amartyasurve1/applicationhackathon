f5b19966fcfd81ca3eafbc02dfb6c01cc6054c21
jaychandak111 <jaychandak111@gmail.com>
2024-07-19 12:26:29 +0530

    animations addded

--- UI/my-app/src/components/Hero.js
+++ UI/my-app/src/components/Hero.js
@@ -1,45 +1,57 @@
-import * as React from 'react';
-import { alpha } from '@mui/material';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Container from '@mui/material/Container';
-import Link from '@mui/material/Link';
-import Stack from '@mui/material/Stack';
-import TextField from '@mui/material/TextField';
-import Typography from '@mui/material/Typography';
+import * as React from "react";
+import { alpha } from "@mui/material";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import Link from "@mui/material/Link";
+import Stack from "@mui/material/Stack";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import MicNoneIcon from "@mui/icons-material/MicNone";
+import SettingsVoiceIcon from "@mui/icons-material/SettingsVoice";
+import imglogo from '../staticimages/animation.gif'
+import imglogo2 from "../staticimages/animation2.gif";
+import "./appli.css";
+import Wave from "react-wavify";
+import { Image } from "@mui/icons-material";
 
 export default function Hero() {
+  const [isMicOpen, setIsMicOpen] = React.useState(false);
+
+  const handleOpenMic = () => {
+    setIsMicOpen(!isMicOpen);
+  };
   return (
     <Box
       id="hero"
       sx={(theme) => ({
-        width: '100%',
+        width: "100%",
         backgroundImage:
-          theme.palette.mode === 'light'
-            ? 'linear-gradient(180deg, #CEE5FD, #FFF)'
-            : `linear-gradient(#02294F, ${alpha('#090E10', 0.0)})`,
-        backgroundSize: '100% 20%',
-        backgroundRepeat: 'no-repeat',
+          theme.palette.mode === "light"
+            ? "linear-gradient(180deg, #CEE5FD, #FFF)"
+            : `linear-gradient(#02294F, ${alpha("#090E10", 0.0)})`,
+        backgroundSize: "100% 20%",
+        backgroundRepeat: "no-repeat",
       })}
     >
       <Container
         sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
           pt: { xs: 14, sm: 20 },
           pb: { xs: 8, sm: 12 },
         }}
       >
-        <Stack spacing={2} useFlexGap sx={{ width: { xs: '100%', sm: '70%' } }}>
+        <Stack spacing={2} useFlexGap sx={{ width: { xs: "100%", sm: "70%" } }}>
           <Typography
             variant="h1"
             sx={{
-              display: 'flex',
-              flexDirection: { xs: 'column', md: 'row' },
-              alignSelf: 'center',
-              textAlign: 'center',
-              fontSize: 'clamp(3.5rem, 10vw, 4rem)',
+              display: "flex",
+              flexDirection: { xs: "column", md: "row" },
+              alignSelf: "center",
+              textAlign: "center",
+              fontSize: "clamp(3.5rem, 10vw, 4rem)",
             }}
           >
             Start&nbsp;
@@ -47,9 +59,11 @@ export default function Hero() {
               component="span"
               variant="h1"
               sx={{
-                fontSize: 'clamp(3rem, 10vw, 4rem)',
+                fontSize: "clamp(3rem, 10vw, 4rem)",
                 color: (theme) =>
-                  theme.palette.mode === 'light' ? 'primary.main' : 'primary.light',
+                  theme.palette.mode === "light"
+                    ? "primary.main"
+                    : "primary.light",
               }}
             >
               Recording.....
@@ -58,7 +72,7 @@ export default function Hero() {
           <Typography
             textAlign="center"
             color="text.secondary"
-            sx={{ alignSelf: 'center', width: { sm: '100%', md: '80%' } }}
+            sx={{ alignSelf: "center", width: { sm: "100%", md: "80%" } }}
           >
             {/* Explore our cutting-edge dashboard, delivering high-quality solutions
             tailored to your needs. Elevate your experience with top-tier features
@@ -66,13 +80,16 @@ export default function Hero() {
             click on the below icon to start the recording
           </Typography>
           <Stack
-            direction={{ xs: 'column', sm: 'row' }}
+            direction={{ xs: "column", sm: "row" }}
             alignSelf="center"
             spacing={1}
             useFlexGap
-            sx={{ pt: 2, width: { xs: '100%', sm: 'auto' } }}
+            sx={{
+              pt: 2,
+              width: { xs: "100%", sm: "auto", alignItems: "center" },
+            }}
           >
-            <TextField
+            {/* <TextField
               id="outlined-basic"
               hiddenLabel
               size="small"
@@ -80,15 +97,34 @@ export default function Hero() {
               aria-label="Enter your email address"
               placeholder="Your email address"
               inputProps={{
-                autoComplete: 'off',
-                'aria-label': 'Enter your email address',
+                autoComplete: "off",
+                "aria-label": "Enter your email address",
               }}
-            />
-            <Button variant="contained" color="primary">
-              Start now
+            /> */}
+            <Button
+              onClick={handleOpenMic}
+              sx={{ width: "150px", height: "150px" }}
+            >
+              <Box>
+                {isMicOpen ? (
+                  <Box width={'100%'}>
+                    <img src={imglogo}></img>
+                  </Box>
+                ) : (
+                  <MicNoneIcon fontSize="large" />
+                )}
+              </Box>    
             </Button>
+            {/* <Button variant="contained" color="primary">
+              Start now
+            </Button> */}
+            {/* {isMicOpen ? : <></>} */}
           </Stack>
-          <Typography variant="caption" textAlign="center" sx={{ opacity: 0.8 }}>
+          <Typography
+            variant="caption"
+            textAlign="center"
+            sx={{ opacity: 0.8 }}
+          >
             By clicking &quot;Start now&quot; you agree to our&nbsp;
             <Link href="#" color="primary">
               Terms & Conditions
@@ -100,24 +136,24 @@ export default function Hero() {
           id="image"
           sx={(theme) => ({
             mt: { xs: 8, sm: 10 },
-            alignSelf: 'center',
+            alignSelf: "center",
             height: { xs: 200, sm: 700 },
-            width: '100%',
+            width: "100%",
             backgroundImage:
-              theme.palette.mode === 'light'
+              theme.palette.mode === "light"
                 ? 'url("/static/images/templates/templates-images/hero-light.png")'
                 : 'url("/static/images/templates/templates-images/hero-dark.png")',
-            backgroundSize: 'cover',
-            borderRadius: '10px',
-            outline: '1px solid',
+            backgroundSize: "cover",
+            borderRadius: "10px",
+            outline: "1px solid",
             outlineColor:
-              theme.palette.mode === 'light'
-                ? alpha('#BFCCD9', 0.5)
-                : alpha('#9CCCFC', 0.1),
+              theme.palette.mode === "light"
+                ? alpha("#BFCCD9", 0.5)
+                : alpha("#9CCCFC", 0.1),
             boxShadow:
-              theme.palette.mode === 'light'
-                ? `0 0 12px 8px ${alpha('#9CCCFC', 0.2)}`
-                : `0 0 24px 12px ${alpha('#033363', 0.2)}`,
+              theme.palette.mode === "light"
+                ? `0 0 12px 8px ${alpha("#9CCCFC", 0.2)}`
+                : `0 0 24px 12px ${alpha("#033363", 0.2)}`,
           })}
         />
       </Container>
